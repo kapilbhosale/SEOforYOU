51b3096e1e74267b0b77986f82433a48e9ca1183
kapil bhosale <kapilbhosale@kapils-MacBook-Pro.local>
2014-02-13 01:01:21 +0530

    Project started

---
+++ SEOforYOU/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SEOforYOU::Application.config.secret_token = '956f69fb421217d16a3d23a5a9ad96e621e6f0fd000333f37ca836ae5137b722678974f2feed76f129679ed5d4d2b8cf3af0f93c5de4aa8781eaafdebdb8b836'
